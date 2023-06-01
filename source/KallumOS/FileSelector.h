a9a0f81f28bb9f14da75c142faaa1de15b45e008
Kallum <kallum179@gmail.com>
2023-06-02 00:32:15 +0100

    :recycle: All to-be shared resources between the file selector and the file saver has been abstracted out into a fileViewer class

--- source/KallumOS/FileSelector.h
+++ source/KallumOS/FileSelector.h
@@ -1,34 +1,13 @@
 #pragma once
+#include "FileViewer.h"
 #include "Helper.h"
 #include "Control.h"
 #include "Button.h"
 
 #include <filesystem>
 
-struct FileOption {
-	std::string fileName;
-	std::filesystem::path filePath;
-	Point position;
-	bool hovered = false;
-	static const int ySize = 20;
-	int fontSize;
-	Color fontColor;
 
-	FileOption(std::string _fileName, int index, Point controlPosition, std::filesystem::path _filePath)
-	{
-		filePath = _filePath;
-		fileName = _fileName;
-		position = controlPosition;
-		position.Add(Point(0, index * ySize));
-
-		fontSize = 10;
-		fontColor = BLACK;
-	}
-
-	void Draw(Point offset);
-};
-
-class FileSelector : public Control {
+class FileSelector : public FileViewer {
 
 public:
 
@@ -45,19 +24,11 @@ public:
 
 private:
 
-	std::filesystem::path path;
-	std::vector<FileOption> currentFiles;
-	std::vector<FileOption> currentDirectories;
 	FileOption* selectedFile;
 
 	void HandleFileClick();
-	void FetchAllCurrentFiles();
-	void DetectFileHover(Point* mousePosition);
-
+	
 	void SetData();
-
-	enum class Operations {selectFile, selectDir, newFile};
-	Operations operation;
 
 	Button submit;
 	bool ready;
